5d4c49af3841b99f6c02e308b222caecc8459a7e
Haseenah01 <haseenahsami27@gmail.com>
2024-04-24 08:18:18 +0000

    modified configs

--- config/test.exs
+++ config/test.exs
@@ -8,15 +8,16 @@ import Config
 config :docker_rel, DockerRel.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
-  database: "docker_rel_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "postgres",
+  database: "docker_rel_test",
+  port: 5432,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :docker_rel, DockerRelWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
+  http: [ip: {0, 0, 0, 0}, port: 4002],
   secret_key_base: "PddPIwJdsKcV/m3pK/P4pbBTADkju4Wo7DtRGU/HewA1toCt8L4zVAbCHQX+uo0b",
   server: false
 
